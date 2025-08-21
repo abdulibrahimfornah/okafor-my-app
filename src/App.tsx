6b18df01065bb47b8dd8ff918306081070d4fffe
Abdul <abdulibrahimfornah@gmail.com>
2025-08-20 19:54:32 -0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+
+import './App.css'
+
+function App() {
+
+  const name = 'Abdul Okafor' 
+
+  return (
+  
+      <>
+      <h1> Hello {name}</h1>
+        
+      
+         </>
+  )
+}
+
+export default App
