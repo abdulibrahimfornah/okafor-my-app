6597193830be622647e12c67055a61cca588e71b
Engr. Wurie <amaduwuriebah3761@gmail.com>
2025-08-21 18:51:00 +0000

    add my name

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,15 @@ import './App.css'
 
 function App() {
 
-  const name = 'Abdul Okafor' 
+  const name = 'Abdul Okafor'
+  const name2 = 'Amadu Wurie' 
 
   return (
   
       <>
       <h1> Hello {name}</h1>
+      <h1>Hello {name2}</h1>
+      
         
       
          </>
